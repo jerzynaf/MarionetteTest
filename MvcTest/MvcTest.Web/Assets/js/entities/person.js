1feced13ed419a29efc965736275a94538df8203
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-02-01 10:35:08 +0000

    Person entity added.

---
+++ MvcTest/MvcTest.Web/Assets/js/entities/person.js
@@ -0,0 +1,52 @@
+﻿PeopleManager.module("Entities", function (Entities, PeopleManager, Backbone, Marionette, $, _) {
+  Entities.Person = Backbone.Model.extend({
+    urlRoot: "api/PeopleApi"
+  });
+
+  Entities.PeopleCollection = Backbone.Collection.extend({
+    url: "api/PeopleApi",
+    model: Entities.Person
+  });
+
+  var API = {
+    getPeopleEntities: function () {
+      var people = new Entities.PeopleCollection();
+      var defer = $.Deferred();
+      people.fetch({
+        success: function (data) {
+          defer.resolve(data);
+        }
+      });
+      var promise = defer.promise();
+      $.when(promise).done(function (fetchedPeople) {
+        if (fetchedPeople.length === 0) {
+          console.log("Empty collection!");
+        }
+      });
+      return promise;
+    },
+
+    getPersonEntity: function (personId) {
+      var person = new Entities.Person({ id: personId });
+      var defer = $.Deferred();
+      person.fetch({
+        success: function (data) {
+          defer.resolve(data);
+        },
+        error: function (data) {
+          defer.resolve(undefined);
+        }
+      });
+
+      return defer.promise();
+    }
+  };
+
+  PeopleManager.reqres.setHandler("people:entities", function () {
+    return API.getPeopleEntities();
+  });
+
+  ContactManager.reqres.setHandler("person:entity", function (id) {
+    return API.getPersonEntity(id);
+  });
+});
